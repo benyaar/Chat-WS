f6095a933929359b4813a59a5a21382784343b4d
Artur <82472015+benyaar@users.noreply.github.com>
2023-03-12 12:33:00 +0200

    feat: add form and input

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from '../styles/Main.module.css'
+import {Link} from "react-router-dom";
+
+const FIELDS = {
+    USERNAME: "username",
+    ROOM: "room"
+}
 
 const Main = () => {
+    const {USERNAME, ROOM} = FIELDS
+
+    const [values, setValues] = useState({[USERNAME]: "", [ROOM]: ""})
+
+    const handleChange = ({target: {value, name}}) => {
+        setValues({...values, [name]: value})
+    }
+    const handleClick = (e) => {
+        const isDisabled = Object.values(values).some((value) => !value)
+        if(isDisabled) e.preventDefault()
+    }
+
     return (
-        <div>Main</div>
+        <div className={styles.wrap}>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>Join</h1>
+                <form className={styles.form}>
+                    <div className={styles.group}>
+                        <input
+                            type="text"
+                            name="username"
+                            value={values[USERNAME]}
+                            placeholder="Enter your name"
+                            className={styles.input}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            required
+                        />
+                    </div>
+                    <div className={styles.group}>
+                        <input
+                            type="text"
+                            name="room"
+                            value={values[ROOM]}
+                            placeholder="Enter name of room"
+                            className={styles.input}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            required
+                        />
+                    </div>
+                    <Link
+                        to={`/chat?name=${values[USERNAME]}&room=${values[ROOM]}`}
+                        className={styles.group}
+                        onClick = {handleClick}
+                    >
+                        <button type="submit" className={styles.button}>
+                            Sign in
+                        </button>
+                    </Link>
+                </form>
+            </div>
+        </div>
     )
 }
 
