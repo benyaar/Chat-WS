009d0ae2113440d38b30cbff2f8f033282933338
Artur <82472015+benyaar@users.noreply.github.com>
2023-03-12 14:23:05 +0200

    fix: server part

--- server/route.js
+++ server/route.js
@@ -3,6 +3,17 @@ import {Router} from "express";
 export const route = Router()
 
 route.get('/', (req, res) => {
-    res.json('Its Work')
-    console.log()
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    res.send("Это только мой мир.");
 })
